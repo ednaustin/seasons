5d1849bda9474788ce9f2b3b28725b2d5bf57543
ednaustin <edramirez512@gmail.com>
2019-09-11 22:32:25 -0500

    added condition to render content

--- src/index.js
+++ src/index.js
@@ -3,15 +3,45 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  constructor(props){
+    super(props);
 
-    return <div>Latitude: </div>;
+    // This is the ONLY time we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // To update our state object, we called setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    );
+  }
+
+  // React says we have to define render
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat} </div>
+    }
+
+    return <div>Loading!</div>
+
   }
 }
+    //return (
+    //  <div>
+    //    Latitude: {this.state.lat}
+    //    <br/>
+    //    Error: {this.state.errorMessage}
+    //  </div>
+  //  );
 
 ReactDOM.render(
   <App />,
